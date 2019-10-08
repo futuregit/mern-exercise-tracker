b951a1877206e29c5ae1f0703a6133e59615e398
futuregit <gmail.com>
2019-10-08 00:54:57 -0400

    Set up web server

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+cors =          require('cors');
+
+//Environment configurations
+
+require('dotenv').config();
+
+// Setting up web server and local port
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middlewares for parsing JSON
+
+app.use(cors());
+app.use(express.json());
+
+// Starting up server to listen on port
+
+app.listen(port, () => {
+    console.log('Sever is running on port ${port}');
+});
